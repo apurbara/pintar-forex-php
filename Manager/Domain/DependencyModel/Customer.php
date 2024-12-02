97d4f35b66330b6913f79cf1f4d2352bc82eb39d
adi purnama <purnama.adi@gmail.com>
2024-12-02 08:27:13 +0700

    fix: [Manager BC] allow city to be fetch from customer entity

--- Manager/Domain/DependencyModel/Customer.php
+++ Manager/Domain/DependencyModel/Customer.php
@@ -65,7 +65,6 @@ class Customer
 
     //QUERY ONLY
     #[FetchableObject(targetEntity: City::class, joinColumnName: "City_id")]
-//    #[ManyToOne(targetEntity: City::class)]
     #[JoinColumn(name: "City_id", referencedColumnName: "id")]
     protected ?City $city;
     
